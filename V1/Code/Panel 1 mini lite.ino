8626f33129e64775fa39f7c7851931a2a0e5ba20
NectariiCh <102548390+NectariiCh@users.noreply.github.com>
2025-06-06 15:13:53 +0300

    Upload files

--- V1/Code/Panel 1 mini lite.ino
+++ V1/Code/Panel 1 mini lite.ino
@@ -11,21 +11,29 @@ bool TOGGLED3 = false;
 #define TRANSLATION_Y A1
 #define TRANSLATION_Z A2
 
-#define TRGAP 2
-#define SASAP 3
-#define RCSAP 4
-#define STGAP 5
-#define AG1AP 6
-#define AG2AP 7
-#define GERAP 8
+#define SASAP 2
+#define RCSAP 3
+#define GERAP 4
+#define BRKAP 5
+#define LITAP 6
+#define AG1AP 7
+#define AG2AP 8
+#define STGAP 9
+#define ABRAP 10
 
-#define TRGMG 7
+#define TRGAP 11
+
 #define SASMG SAS_ACTION
 #define RCSMG RCS_ACTION
-#define STGMG STAGE_ACTION
+#define GERMG GEAR_ACTION
+#define BRKMG BRAKES_ACTION
+#define LITMG LIGHT_ACTION
 #define AG1MG 1
 #define AG2MG 2
-#define GERMG GEAR_ACTION
+#define STGMG STAGE_ACTION
+#define ABRMG ABORT_ACTION
+
+#define TRGMG 7
 
 #define THC_X_DEADZONE_MIN (511 - 20)
 #define THC_X_DEADZONE_MAX (511 + 20)
@@ -35,6 +43,13 @@ bool TOGGLED3 = false;
 #define THC_Z_DEADZONE_MAX (511 + 20)
 
 KerbalSimpit mySimpit(Serial);
+
+bool checkBtnWDebounce(int pin, int time) {
+  if (!digitalRead(pin)) {
+    delay(time);
+    if (!digitalRead(pin)) return true; else return false;
+  } else return false;
+}
 
 void checkAGs() {
   // TRG
@@ -59,7 +74,7 @@ void checkAGs() {
   }
 
   // STG
-  if (!digitalRead(STGAP)) {
+  if (checkBtnWDebounce(STGAP, 10)) {
      if (!TOGGLED3) {
       mySimpit.activateAction(STGMG);
      }
@@ -70,9 +85,10 @@ void checkAGs() {
 
   // AG1
   if (CAG_MODE_TOGGLE) {
-    if (!digitalRead(AG1AP)) {
+    if (checkBtnWDebounce(AG1AP, 10)) {
      if (!TOGGLED1) {
       mySimpit.toggleCAG(AG1MG);
+      TOGGLED1 = true;
      }
     } else {
       TOGGLED1 = false;
@@ -87,9 +103,10 @@ void checkAGs() {
 
   // AG2
   if (CAG_MODE_TOGGLE) {
-    if (!digitalRead(AG2AP)) {
+    if (checkBtnWDebounce(AG2AP, 10)) {
      if (!TOGGLED2) {
       mySimpit.toggleCAG(AG2MG);
+      TOGGLED2 = true;
      }
     } else {
       TOGGLED2 = false;
@@ -108,6 +125,27 @@ void checkAGs() {
   } else {
     mySimpit.deactivateAction(GERMG);
   }
+
+  // BRK
+  if (!digitalRead(BRKAP)) {
+    mySimpit.activateAction(BRKMG);
+  } else {
+    mySimpit.deactivateAction(BRKMG);
+  }
+
+  // LIT
+  if (!digitalRead(LITAP)) {
+    mySimpit.activateAction(LITMG);
+  } else {
+    mySimpit.deactivateAction(LITMG);
+  }
+
+  // ABR
+  if (!digitalRead(ABRAP)) {
+    mySimpit.activateAction(ABRMG);
+  } else {
+    mySimpit.deactivateAction(ABRMG);
+  }
 }
 
 void setup() {
@@ -120,6 +158,9 @@ void setup() {
   pinMode(AG1AP, INPUT_PULLUP);
   pinMode(AG2AP, INPUT_PULLUP);
   pinMode(GERAP, INPUT_PULLUP);
+  pinMode(BRKAP, INPUT_PULLUP);
+  pinMode(LITAP, INPUT_PULLUP);
+  pinMode(ABRAP, INPUT_PULLUP);
   pinMode(TRGAP, INPUT_PULLUP);
   pinMode(TRANSLATION_X, INPUT);
   pinMode(TRANSLATION_Y, INPUT);
@@ -157,8 +198,8 @@ void loop() {
   int reading = analogRead(A2);
 
   rotationMessage translation_msg;
-  translation_msg.setPitch(translationX);
-  translation_msg.setRoll(translationY);
+  translation_msg.setYaw(-translationX);
+  translation_msg.setPitch(-translationY);
   mySimpit.send(ROTATION_MESSAGE, translation_msg);
   throttleMessage throttle_msg;
   throttle_msg.throttle = map(reading, 1023, 630, 0, INT16_MAX); 
